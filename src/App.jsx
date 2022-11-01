b845bd3f33ca954b442ed6e93e9dc86cbacaaec7
arslonga83 <jlwood83@gmail.com>
2022-11-01 16:28:14 -0700

    hook up quiz button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import React from 'react'
 import './App.css'
 import IntroPage from './Components/IntroPage'
+import QuizPage from './Components/QuizPage'
 
 function App() {
- 
-  return (
-   <IntroPage />
-  )
+
+  const [playGame, setPlayGame] = React.useState(false)
+
+  function startQuiz() {
+    setPlayGame(true)
+  }
+
+return (
+  playGame ? <QuizPage /> : <IntroPage startQuiz={startQuiz}/>
+)
+
+
+
 }
 
 export default App
---
+++ src/Components/QuizPage.jsx
@@ -0,0 +1,7 @@
+export default function QuizPage() {
+  return (
+    <div>
+      <h1>Quiz Time</h1>
+    </div>
+  )
+}
