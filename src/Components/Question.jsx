d6b4e9ea893b1f53d866c4610ef4408edf565164
arslonga83 <jlwood83@gmail.com>
2022-11-01 20:19:47 -0700

    toggle selected style on click

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -1,12 +1,12 @@
-export default function Question() {
+export default function Question(props) {
   return (
     <div className='question'>
       <h4>What is the best band in the world?</h4>
       <div className='choices'>
-        <button className='choice selected'>BTS</button>
-        <button className='choice'>Green Day</button>
-        <button className='choice'>The Beatles</button>
-        <button className='choice'>Metallica</button>
+        <button className='choice selected' onClick={props.handleClick}>BTS</button>
+        <button className='choice' onClick={props.handleClick}>Green Day</button>
+        <button className='choice' onClick={props.handleClick}>The Beatles</button>
+        <button className='choice' onClick={props.handleClick}>Metallica</button>
       </div>
       <hr></hr>
     </div>
--- src/Components/QuizPage.jsx
+++ src/Components/QuizPage.jsx
@@ -4,15 +4,20 @@ import blob4 from '../assets/blob4.png'
 import Question from './Question'
 
 export default function QuizPage() {
+
+  function handleClick(event) {
+    event.target.classList.toggle('selected')
+  }
+
   return (
     <main>
       <img src={blob3} className='blob1'></img>
         <div className='quiz-content'>
-        <Question />
-        <Question />
-        <Question />
-        <Question />
-        <Question />
+        <Question handleClick={handleClick}/>
+        <Question handleClick={handleClick}/>
+        <Question handleClick={handleClick}/>
+        <Question handleClick={handleClick}/>
+        <Question handleClick={handleClick}/>
         <button className='check' onClick={() => console.log('you rock')}>Check answers</button>
         </div>
       <img src={blob4} className='blob2'></img>
