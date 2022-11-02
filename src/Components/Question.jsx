8edc21d9be119678308efa7e25cc537f93811023
arslonga83 <jlwood83@gmail.com>
2022-11-01 20:02:57 -0700

    style quiz page

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -1,12 +1,12 @@
 export default function Question() {
   return (
     <div className='question'>
-      <h4>Test question</h4>
-      <div>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
+      <h4>What is the best band in the world?</h4>
+      <div className='choices'>
+        <button className='choice selected'>BTS</button>
+        <button className='choice'>Green Day</button>
+        <button className='choice'>The Beatles</button>
+        <button className='choice'>Metallica</button>
       </div>
       <hr></hr>
     </div>
